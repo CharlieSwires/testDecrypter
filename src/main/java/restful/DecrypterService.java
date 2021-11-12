59054ae221cb19051f262c1e5629329167497a08
CharlieSwires <charlieswires@gmail.com>
2021-11-12 20:06:07 +0000

    Single thread pagination

--- src/main/java/restful/DecrypterService.java
+++ src/main/java/restful/DecrypterService.java
@@ -19,34 +19,40 @@ public class DecrypterService {
     private SecondaryMongoBeanRepository secondaryRepository;
 
     public Boolean start() {
-        final String uri = "http://container2:8080/address-book/AddressEntry/getAll";
-        RestTemplate restTemplate = new RestTemplate(); 
-        String result = restTemplate.getForObject( uri, String.class);
+        final String uri = "http://container2:8080/address-book/AddressEntry/getAllArray";
+        RestTemplate restTemplate = new RestTemplate();
+        int page = 0;
+        Boolean finished = false;
+        while(!finished) {
+            String result = restTemplate.getForObject( uri+"/"+(page++), String.class);
+            finished =storeBatch(result);
+        }
+        return true;
+    }
+    
+    private Boolean storeBatch(String result) {
         ObjectMapper m = new ObjectMapper();
         ResponseBean1[] products = null;
         try {
             products = m.readValue(result, ResponseBean1[].class);
-       for (ResponseBean1 item : products) {
-           secondary.SecondaryMongoBean newItem = new SecondaryMongoBean();
-           newItem.setAddress(item.getAddress());
-           newItem.setFirstname(item.getFirstname());
-           newItem.setHomeTel(item.getHomeTel());
-           newItem.setMobile(item.getMobile());
-           newItem.setPersonalEmail(item.getPersonalEmail());
-           newItem.setSurname(item.getSurname());
-           newItem.setTitle(item.getTitle());
-           newItem.setWorkEmail(item.getWorkEmail());
-           newItem.setWorkTel(item.getWorkTel());
-           this.secondaryRepository.save(newItem);
-       }
-        } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            for (ResponseBean1 item : products) {
+                secondary.SecondaryMongoBean newItem = new SecondaryMongoBean();
+                newItem.setAddress(item.getAddress());
+                newItem.setFirstname(item.getFirstname());
+                newItem.setHomeTel(item.getHomeTel());
+                newItem.setMobile(item.getMobile());
+                newItem.setPersonalEmail(item.getPersonalEmail());
+                newItem.setSurname(item.getSurname());
+                newItem.setTitle(item.getTitle());
+                newItem.setWorkEmail(item.getWorkEmail());
+                newItem.setWorkTel(item.getWorkTel());
+                this.secondaryRepository.save(newItem);
+            }
+        } catch (Exception e) {
+            return true;
         }
-
-       return true;
+        return false;
     }
-    
     public Integer stop() {
         List<SecondaryMongoBean> list = this.secondaryRepository.findAll();
         return list.size();
